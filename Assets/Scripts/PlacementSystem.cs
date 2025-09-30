5bf5c6000b24f3266b2a465f76d8fa395731560e
astarlulu <zoewaluni@gmail.com>
2025-10-01 12:39:02 +1300

    StartInsideMaisieHome
    Completed Tutorial One by Sunny Valley Studio

---
+++ Assets/Scripts/PlacementSystem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    GameObject mouseIndicator, cellIndicator; 
+    [SerializeField]
+    private InputManager inputManager;
+    [SerializeField]
+    private Grid grid;
+
+    private void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+}
